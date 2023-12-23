84788ef65f79b204f4d21304365553d7c1916dc4
MarinaBajic <marinabajic10@gmail.com>
2023-12-24 00:31:37 +0100

    custom exception handling

---
+++ reo-microservices/horse-service/src/main/java/com/reo/horseservice/exception/CustomResponseEntityExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.reo.horseservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(EntityAlreadyExistsException.class)
+    public ResponseEntity<ErrorEntity> handleEntityAlreadyExists(EntityAlreadyExistsException ex) {
+        ErrorEntity errorEntity = new ErrorEntity(ex.getMessage(), ex.getIdEntity());
+        return new ResponseEntity<>(errorEntity, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(EntityDoesNotExistException.class)
+    public ResponseEntity<ErrorEntity> handleEntityDoesNotExists(EntityDoesNotExistException ex) {
+        ErrorEntity errorEntity = new ErrorEntity(ex.getMessage(), ex.getIdEntity());
+        return new ResponseEntity<>(errorEntity, HttpStatus.BAD_REQUEST);
+    }
+
+}
