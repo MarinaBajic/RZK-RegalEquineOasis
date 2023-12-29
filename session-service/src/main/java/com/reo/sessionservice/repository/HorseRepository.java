47e7656f879e9320f1ffb5c2d8f045d4afb66b98
Marina Bajić <41272817+MarinaBajic@users.noreply.github.com>
2023-12-29 18:01:50 +0100

    Delete session-service directory

--- session-service/src/main/java/com/reo/sessionservice/repository/HorseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.reo.sessionservice.repository;
-
-import com.reo.sessionservice.model.Horse;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HorseRepository extends JpaRepository<Horse, Integer> {
-}
